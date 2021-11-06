6884c2e04d470a6620d8858f0eb9ebfd87a543d4
plvkuzn <pvl.kuzn@gmail.com>
2021-11-06 16:44:10 +0500

    controllers dont complite

--- controllers/counterValueController.js
+++ controllers/counterValueController.js
@@ -1,3 +1,5 @@
+const { CounterValue } = require("../models/models")
+
 class CounterValueController {
     async createNew(req, res, next){
     
@@ -5,15 +7,23 @@ class CounterValueController {
     async update(req, res, next){
   
     }
-    async getAll(res,req){
-  
+    async getAll(req, res, next){
+      const counterValuesAll = await CounterValue.findAll()
+      return res.json(counterValuesAll)
     }
-    async getMonth(res,req){
-  
+    async getMonth(req, res, next){
+      const p  = req.body //{period}
+      console.log(p)
+      const allValuesMonth = await CounterValue.findAll({
+        where : {period: '2021-01-01'} 
+      })
+      return res.json(allValuesMonth)
     }
 
-    async getOne(res,req){
-  
+    async getOne(req, res, next){
+      const {id} = req.params
+      const valueFounded = await CounterValue.findOne({where : {id}}) 
+      return res.json(valueFounded)
     }
    
   }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,42 @@
+const { User } = require('../models/models')
+
 ApiError = require('../error/ApiError')
 class UserController {
   async createNew(req,res, next){
-      const {email, password} = req.body
+      const {email, password, role, name, phone, desc} = req.body
       if (!email || !password){
           return console.log('Incorrect email or password') // первая валидация на клиенте
       }
+      const newUser = await User.create({email, password, role, name, phone, desc})
+      return res.json(newUser)
+/*
+      id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement:true},
+      email: {type: DataTypes.STRING, unique: true, allowNull: false },       // уникальный логин
+      password: {type: DataTypes.STRING, allowNull: false},              // шифровние
+      role: {type: DataTypes.STRING, defaultValue : "USER"}, // или ссылка на таблицу с ролями или хардкод?
+      name:{type: DataTypes.STRING, allowNull: false },          // фио
+      phone:{type: DataTypes.STRING,},         //  номер телфона
+      desc:{type: DataTypes.STRING,}           // доп инфа (?) нужна ли
+
+*/
   }
   async login(req, res, next){
     res.json('llllllllllll')
   }
   async getAll(req, res, next){
-    res.json('43333')
+    const users = await User.findAll({
+      attributes : {exclude : ['password']}
+    })
+    res.json(users)
   }
 
   async getOne(req,res, next){
-    res.json('11111')
+    const {id} = req.params
+    const userFounded = await User.findOne({
+      attributes : {exclude : ['password']},
+      where : {id}}) 
+    return res.json(userFounded)
+    
   }
 
   async check(req, res, next){
